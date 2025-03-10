edd310e0fb5ce430f1ee40a8f00308491b9ebb2b
Saksham Verma <sakshamverma1000@gmail.com>
2025-03-10 21:29:53 +0530

    Used predefined middlewares in app, also created and used custom built errorMiddleware

--- app.js
+++ app.js
@@ -4,11 +4,21 @@ import userRouter from "./routes/user.routes.js";
 import subscriptionRouter from "./routes/subscription.routes.js";
 import authRouter from "./routes/auth.routes.js";
 import connectToDB from "./database/mongodb.js";
+import errorMiddleware from "./middlewares/error.middleware.js";
+import cookieParser from "cookie-parser";
 const app = express();
+
+// Pre Defined Express.js Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/subscriptions", subscriptionRouter);
+
+// Error Middleware
+app.use(errorMiddleware);
 
 app.get("/", (req, res) => {
   res.send("Welcome to the Subscription Tracker API !");
